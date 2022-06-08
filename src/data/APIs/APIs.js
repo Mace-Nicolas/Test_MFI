d5a372ed8027ab6ee1dd8be79af9a19eb7f4f50d
Nicolas M <nicolas.mace@insidegroup.fr>
2022-06-08 10:20:59 +0200

    changed API key open weather

--- src/data/APIs/APIs.js
+++ src/data/APIs/APIs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getForecastFromLonLat = async (lon, lat) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=539a92a71fbb1b6ee46f8afdfc95bb2e&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=8d099a68a8059919509fc3e2691a7c46&units=metric`;
   const response = await axios.get(url);
   const data = response.data;
   const dailyTemps = data.daily;
